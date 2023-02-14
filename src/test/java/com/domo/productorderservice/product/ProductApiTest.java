b33abe42cda18de637f087658035272f211c78de
kdomo <dongho5088@naver.com>
2023-02-14 20:50:27 +0900

    test: 상품조회 API 테스트로 변경

--- src/test/java/com/domo/productorderservice/product/ProductApiTest.java
+++ src/test/java/com/domo/productorderservice/product/ProductApiTest.java
@@ -9,6 +9,10 @@ import org.springframework.http.HttpStatus;
 
 import com.domo.productorderservice.ApiTest;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
 public class ProductApiTest extends ApiTest {
 	@Autowired
 	private ProductService productService;
@@ -20,5 +24,23 @@ public class ProductApiTest extends ApiTest {
 		final var response = ProductSteps.상품등록요청(request);
 
 		assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+	}
+
+	@Test
+	@DisplayName("상품 조회")
+	void 상품조회() {
+		productService.addProduct(ProductSteps.상품등록요청_생성());
+		Long productId = 1L;
+
+		final ExtractableResponse<Response> response = RestAssured.given().log().all()
+				.when()
+				.get("/products/{productId}", productId)
+				.then().log().all()
+				.extract();
+
+		assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+		assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
+		assertThat(response.jsonPath().getInt("price")).isEqualTo(1000);
+		assertThat(response.jsonPath().getString("discountPolicy")).isEqualTo("NONE");
 	}
 }
