82a3228de2c29cf4f7989c9024fba98ca34e4103
kdomo <dongho5088@naver.com>
2023-02-14 15:57:03 +0900

    chore: Jpa적용하기 - 기존 ProductRepository에서 memory에 저장되던 값을 JpaRepository를 활용하여 H2 데이터베이스에 저장되도록 변경 - 테스트를 각각 격리시키기 위해 BeforeEach로 DataBaseCleanUp을 시켜 id값이 1부터 시작하도록 설정

--- src/main/java/com/domo/productorderservice/product/Product.java
+++ src/main/java/com/domo/productorderservice/product/Product.java
@@ -1,16 +1,31 @@
 package com.domo.productorderservice.product;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.springframework.util.Assert;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "products")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
 
-	private final String name;
-	private final int price;
-	private final DiscountPolicy discountPolicy;
+	@Id
+	@GeneratedValue(strategy = javax.persistence.GenerationType.IDENTITY)
 	private Long id;
+
+	private String name;
+
+	private int price;
+
+	private DiscountPolicy discountPolicy;
 
 	public Product(final String name, final int price, final DiscountPolicy discountPolicy) {
 		Assert.hasText(name, "상품명은 필수입니다.");
@@ -20,8 +35,6 @@ public class Product {
 		this.price = price;
 		this.discountPolicy = discountPolicy;
 	}
-
-	public void assignId(Long id) {
-		this.id = id;
-	}
 }
+
+
--- src/main/java/com/domo/productorderservice/product/ProductRepository.java
+++ src/main/java/com/domo/productorderservice/product/ProductRepository.java
@@ -1,17 +1,8 @@
 package com.domo.productorderservice.product;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ProductRepository {
-	private Long sequence = 0L;
-	private Map<Long, Product> persistence = new HashMap<>();
-
-	public void save(final Product product) {
-		product.assignId(++sequence);
-		persistence.put(product.getId(), product);
-	}
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
